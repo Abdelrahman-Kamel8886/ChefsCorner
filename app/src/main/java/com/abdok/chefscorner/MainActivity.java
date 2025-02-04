e5cd2ef61707638c33b8f1efa0cda85c38832c07
Abdelrahman-Kamel8886 <abdelrahmankamel8886@gmail.com>
2025-02-04 17:43:21 +0200

    add curved navigation view

--- app/src/main/java/com/abdok/chefscorner/MainActivity.java
+++ app/src/main/java/com/abdok/chefscorner/MainActivity.java
@@ -1,6 +1,8 @@
 package com.abdok.chefscorner;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.abdok.chefscorner.databinding.ActivityMainBinding;
+
+import np.com.susanthapa.curved_bottom_navigation.CbnMenuItem;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        @SuppressLint("ResourceType") CbnMenuItem[] menuItems = new CbnMenuItem[] {
+                new CbnMenuItem(R.drawable.ic_baseline_bookmark_border_24, R.drawable.ic_baseline_bookmark_avd,1),
+                new CbnMenuItem(R.drawable.ic_baseline_bookmark_border_24, R.drawable.ic_baseline_bookmark_avd,2),
+                new CbnMenuItem(R.drawable.ic_baseline_bookmark_border_24, R.drawable.ic_baseline_bookmark_avd,3)
+        };
+        binding.nav.setMenuItems(menuItems, 0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
