a162e0f25b5b1aa72927b0c0b0d45f0dbcc38e48
Abdelrahman-Kamel8886 <abdelrahmankamel8886@gmail.com>
2025-02-06 08:01:21 +0200

    lottie animation to splash screen

---
+++ app/src/main/java/com/abdok/chefscorner/Ui/Auth/SignUp/RegiesterFragment.java
@@ -0,0 +1,21 @@
+package com.abdok.chefscorner.Ui.Auth.SignUp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.abdok.chefscorner.R;
+
+
+public class RegiesterFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_regiester, container, false);
+    }
+}
